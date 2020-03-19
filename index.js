0b9fc43872617539eb8933b8dedc62bd75a08fad
Andy Bui <andy.bui2001@gmail.com>
2020-03-19 23:01:39 +1100

    add stacked graph: condition status vs time

--- index.js
+++ index.js
@@ -42,20 +42,28 @@ function plusRecover() {
 
 function plusTime() {
     data[time] = {
-        x: time,
-        y: HEALTHY_COUNT
+        time: time,
+        recovered: RECOVERED_COUNT,
+        infected: INFECTED_COUNT,
+        healthy: HEALTHY_COUNT
+        
     }
     time++;
+    // Refresh graph
+    d3.select("div#reload").select("div#chart").remove();
+    d3.select("div#reload").append("div").attr("id", "chart");
+
+    draw();
 }
 
 // Ball settings
 const BALL_RADIUS = 5;
 
+const HEALTHY = 'lightblue';
 const INFECTED = 'orange';
 const RECOVERED = 'pink';
-const HEALTHY = 'lightblue';
 
-const BALL_COLORS = [INFECTED, HEALTHY];
+const BALL_COLORS = [RECOVERED, HEALTHY, INFECTED];
 
 // Canvas settings
 const canvasWidth = 900;
@@ -198,7 +206,7 @@ function recover() {
         
         if (ball.infected == false || ball.recovered == true) continue;
 
-        if (tick_count - ball.tick_infected >= 1000) {
+        if (tick_count - ball.tick_infected >= 500) {
             ball.infected = false;
             ball.recovered = true;
             d3.select("circle#ball"+i.toString()).style("fill", RECOVERED);
@@ -246,10 +254,6 @@ function tick(state) {
     d3.select("span.time-count").text(time.toString());
 
     tick_count++;
-
-    if (time>=50) {
-        draw();
-    }
 }
 
 function render() {
@@ -268,4 +272,89 @@ function render() {
         });
     }, 800);
 
+}
+
+function draw() {
+
+    // Create SVG and padding for the chart
+    const svg = d3
+        .select("#chart")
+        .append("svg")
+        .attr("height", 200)
+        .attr("width", 400);
+
+    const strokeWidth = 0;
+    const margin = { top: 0, bottom: 20, left: 30, right: 20 };
+    const chart = svg.append("g").attr("transform", `translate(${margin.left},0)`);
+
+    const width = +svg.attr("width") - margin.left - margin.right - strokeWidth * 2;
+    const height = +svg.attr("height") - margin.top - margin.bottom;
+    const grp = chart
+        .append("g")
+        .attr("transform", `translate(-${margin.left - strokeWidth},-${margin.top})`);
+
+    // Create stack
+    const stack = d3.stack().keys(["recovered", "healthy", "infected"]);
+    const stackedValues = stack(data);
+    const stackedData = [];
+    // Copy the stack offsets back into the data.
+    stackedValues.forEach((layer, index) => {
+        const currentStack = [];
+        layer.forEach((d, i) => {
+            currentStack.push({
+                values: d,
+                time: data[i].time
+            });
+        });
+        stackedData.push(currentStack);
+    });
+
+    // Create scales
+    const yScale = d3
+        .scaleLinear()
+        .range([0, height])
+        .domain([0, BALL_COUNT]);
+    const xScale = d3
+        .scaleLinear()
+        .range([0, width])
+        .domain([0,BALL_COUNT*2]);
+
+    const area = d3
+        .area()
+        .x(dataPoint => xScale(dataPoint.time))
+        .y0(dataPoint => yScale(dataPoint.values[0]))
+        .y1(dataPoint => yScale(dataPoint.values[1]));
+
+
+
+    // Actual draw area
+    
+    const series = grp
+    .selectAll(".series")
+    .data(stackedData)
+    .enter()
+    .append("g")
+    .attr("class", "series");
+    console.log(stackedData);
+
+
+    series.append("path")
+    .attr("transform", `translate(${margin.left},0)`)
+    .style("fill", (d, i) => BALL_COLORS[i])
+    .attr("stroke", "steelblue")
+    .attr("stroke-linejoin", "round")
+    .attr("stroke-linecap", "round")
+    .attr("stroke-width", strokeWidth)
+    .attr("d", d => area(d));
+
+    // Add the X Axis
+    chart
+        .append("g")
+        .attr("transform", `translate(0,${height})`);
+
+    // Add the Y Axis
+    chart
+        .append("g")
+        .attr("transform", `translate(0, 0)`);
+    
 }
